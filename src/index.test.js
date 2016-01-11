5660189cc5483f0be39dac996ce95b91a79b49ef
Bogdan Savluk <savluk.bogdan@gmail.com>
2016-01-12 00:10:45 +0200

    cover all sources

--- src/index.test.js
+++ src/index.test.js
@@ -1,2 +1,3 @@
-let testsContext = require.context('.', true, /\.test$/);
+// load all js sources
+let testsContext = require.context('./demo', true, /\.js$/);
 testsContext.keys().forEach(testsContext);
